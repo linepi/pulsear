a0e70f544ab078bd138e5b84e66401d5f5177197
linepi <2428513224@qq.com>
2024-08-02 14:25:08 +0800

    login and logout

--- pulsear/src/api.rs
+++ pulsear/src/api.rs
@@ -1,16 +1,23 @@
-use actix_web::{get, post, web, HttpResponse};
+use actix_web::{get, post, web, HttpResponse, HttpRequest};
+use crate::*;
 
-pub async fn index() -> HttpResponse {
-  let res = std::fs::read_to_string("pulsear-ui/ui/index.html")
+pub async fn index(req: HttpRequest) -> HttpResponse {
+  // like 192.168.31.126:4444
+  let my_ipaddr = req.headers().get("host").unwrap().to_str().unwrap().to_string();
+  log::info!("my_ipaddr: {}", my_ipaddr);
+  let mut html_str = std::fs::read_to_string("pulsear-ui/ui/index.html")
                     .unwrap_or_else(|e| {
                         println!("error: {} of index.html", e);
                         e.to_string()
                     });
-  HttpResponse::Ok().body(res)
+  // replace the ipaddr in html_str to actual one 
+  html_str = html_str.replace("giocdanewla", format!("http://{}/", &my_ipaddr).as_str());
+  HttpResponse::Ok().body(html_str)
 }
 
 #[get("/resources/{path}")]
 pub async fn resources(path: web::Path<String>) -> HttpResponse {
+  log::info!("visit path {}", path);
   let res = std::fs::read_to_string(format!("pulsear-ui/ui/{}", path))
                     .unwrap_or_else(|e| {
                         println!("error: {} of {}", e, path);
@@ -19,30 +26,47 @@ pub async fn resources(path: web::Path<String>) -> HttpResponse {
   HttpResponse::Ok().body(res)
 }
 
-#[derive(serde::Deserialize, Hash)]
-struct LoginInfo {
-  username: String,
-  password: String
-}
-
-#[derive(serde::Serialize)]
-struct UserCtx {
-  username: String,
-  token: String,
-}
-
-fn gen_random_token<LoginInfo: std::hash::Hash>(login_info: LoginInfo) -> String {
-  use std::hash::{Hasher, DefaultHasher};
-  let mut hasher = DefaultHasher::new();
-  login_info.hash(&mut hasher);
-  hasher.finish().to_string()
-}
-
 #[post("/login")]
-pub async fn login(param: web::Json<LoginInfo>) -> HttpResponse {
-  log::info!("login in username: {}", param.username);
-  HttpResponse::Ok().json(UserCtx {
-    username: param.username.clone(), 
-    token: gen_random_token(param.into_inner())
-  })
+pub async fn login(param: web::Json<LoginRequest>) -> HttpResponse {
+  log::info!("user login: {}", serde_json::to_string(&param).unwrap());
+  let login_response: LoginResponse;
+  match &param.login_info.choice {
+    LoginChoice::Token(token) => {
+      login_response = LoginResponse {
+        user_ctx: UserCtx {
+          username: param.login_info.username.clone(), 
+          token: token.clone(),
+        },
+        basic_info: StreamBasicInfo {
+          time_stamp: get_system_timestamp_milli()
+        },
+        code: ResponseCode::Ok
+      };
+    },
+    LoginChoice::Password(_) => {
+      login_response = LoginResponse {
+        user_ctx: UserCtx {
+          username: param.login_info.username.clone(), 
+          token: gen_random_token(&param.login_info)
+        },
+        basic_info: StreamBasicInfo {
+          time_stamp: get_system_timestamp_milli()
+        },
+        code: ResponseCode::Ok
+      };
+    }
+  }
+  HttpResponse::Ok().json(login_response)
+}
+
+#[post("/logout")]
+pub async fn logout(param: web::Json<LogoutRequest>) -> HttpResponse {
+  log::info!("user logout: {}", serde_json::to_string(&param).unwrap());
+  let logout_response = LogoutResponse {
+    basic_info: StreamBasicInfo {
+      time_stamp: get_system_timestamp_milli()
+    },
+    code: ResponseCode::Ok
+  };
+  HttpResponse::Ok().json(logout_response)
 }
--- pulsear/src/main.rs
+++ pulsear/src/main.rs
@@ -9,15 +9,18 @@ struct Args {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  env_logger::init();
+  env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 	HttpServer::new(|| {
       App::new()
+      .wrap(actix_web::middleware::Logger::default())
       .route("/", web::get().to(api::index))
       .route("/index.html", web::get().to(api::index))
       .service(api::resources)
       .service(api::login)
+      .service(api::logout)
     })
-		.bind(("127.0.0.1", 6543))?
+		.bind(("0.0.0.0", 6543))?
+    .workers(4)
 		.run()
 		.await
 }
